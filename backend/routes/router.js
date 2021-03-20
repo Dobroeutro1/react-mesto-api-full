adaef81052b6d215c95f9ab978fd95bbe0e2951f
Artem Noskov <artyom.noskov.1998@yandex.ru>
2021-03-20 17:08:06 +0500

    Добавил мидлвэр для авторизации

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,11 +1,12 @@
-const router = require('express').Router();
-const cardsRouter = require('./cards');
-const usersRouter = require('./users');
+/* eslint-disable linebreak-style */
+const router = require("express").Router();
+const cardsRouter = require("./cards");
+const usersRouter = require("./users");
 
-router.use('/cards', cardsRouter);
-router.use('/users', usersRouter);
-router.use('*', (req, res) => {
-  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+router.use("/cards", cardsRouter);
+router.use("/users", usersRouter);
+router.use("*", (req, res) => {
+  res.status(404).send({ message: "Запрашиваемый ресурс не найден" });
 });
 
 module.exports = router;
